d155651d2c7befcc87ddd5d3fc576362015ba908
vedha ponnam <245122749040@mvsrec.edu.in>
2025-03-25 15:45:00 +0530

    Add files via upload

--- Add.java
+++ Add.java
@@ -1,18 +1,11 @@
 import java.util.Scanner;
 public class Add {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter first number: ");
-        int num1 = scanner.nextInt();
-
-        System.out.print("Enter second number: ");
-        int num2 = scanner.nextInt();
-
-        int sum = num1 + num2;
+        int num1 = 10
+       int num2 = 20
+       int sum = num1 + num2;
         System.out.println("Sum: " + sum);
 
-        scanner.close();
     }
 }
 
